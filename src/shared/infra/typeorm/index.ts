68fbd608e79595f523eab6c350f42a9ce7bf553d
EduardoGrangeiro2002 <eduatdoaugusto503@gmail.com>
2022-04-03 09:31:25 -0300

    fix: corrigindo connect database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,15 +1,13 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-
-export default async(host = "database") :Promise<Connection> => {
-  const defaultOptions = await getConnectionOptions()
+export default async (): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
-        ? "rentx_test"
-        : defaultOptions.database
+          ? "rentx_test"
+          : defaultOptions.database,
     })
   );
 };
